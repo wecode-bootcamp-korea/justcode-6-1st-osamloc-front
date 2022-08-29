c5e7de82d52e3b84e7539c7d2a34baea38c13fb0
sh.inv <qierapu1im@gmail.com>
2022-08-29 15:23:04 +0900

    Add: puduct  brach 테스트

---
+++ src/pages/Product/Product.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Product() {
+  return (
+    <h1>제품 상세페이지 테스트</h1>
+  )
+}
+
+export default Product;
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Main from './Main/Main';
 import Login from './Login/Login';
+import Product from './Product/Product';
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
