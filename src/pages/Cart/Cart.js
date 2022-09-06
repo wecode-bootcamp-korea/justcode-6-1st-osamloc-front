0c413024655220648a3915e677802e104ec73236
yoon <yoon@yoonui-MacBookAir.local>
2022-09-06 22:29:06 +0900

    add: footer ui 수정

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import CartForm from "../../components/Cart/CartForm";
 import "./Cart.scss";
-import Footer from '../../components/Footer/Footer';
 
 function Cart() {
   return (
@@ -19,7 +18,6 @@ function Cart() {
           <CartForm />
         </main>
       </div>
-      <Footer />
     </>
   );
 }
