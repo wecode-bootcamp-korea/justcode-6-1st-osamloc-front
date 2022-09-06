5ad39ffa9f9976b05daca6971ef2e324dd52b26e
yoon <yoon@yoonui-MacBookAir.local>
2022-09-06 22:23:55 +0900

    add: footer ui 구현

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
 import CartForm from "../../components/Cart/CartForm";
 import "./Cart.scss";
+import Footer from '../../components/Footer/Footer';
 
 function Cart() {
   return (
@@ -19,7 +19,7 @@ function Cart() {
           <CartForm />
         </main>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
